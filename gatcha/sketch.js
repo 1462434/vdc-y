61a3e853888b66a0373f5c456fa559a78afe14ae
Pushpull-Kirby <1462434@snu.ac.kr>
2025-09-17 21:56:03 +0900

    랜덤 얼굴 완성!
    function draw() {
      background(220);
    }
    
    이걸 지우니까 됐음

--- gatcha/sketch.js
+++ gatcha/sketch.js
@@ -1,7 +1,20 @@
-function setup() {
-  createCanvas(400, 400);
+let body;
+let face;
+
+//커비 몸통 얼굴 이미지 불러오기
+function preload() {
+  body = loadImage('body.png');
+
+  // select a random image
+  face = loadImage(int(random(1, 5)) + ".png");  
 }
 
-function draw() {
-  background(220);
+// 커비 몸통 얼굴 일단 표출
+function setup() {
+  createCanvas(400, 400);
+  background('beige');
+  image(body, 0, 0);
+  image(face, 132, 134);
 }
+
+
