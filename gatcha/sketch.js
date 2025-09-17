49ef5d020110e72c45f3df68630752bead869b32
Pushpull-Kirby <1462434@snu.ac.kr>
2025-09-17 22:02:53 +0900

    랜덤 얼굴 버튼 생성

--- gatcha/sketch.js
+++ gatcha/sketch.js
@@ -1,20 +1,33 @@
 let body;
 let face;
 
-//커비 몸통 얼굴 이미지 불러오기
+// 커비 몸통 얼굴 이미지 불러오기
 function preload() {
   body = loadImage('body.png');
 
-  // select a random image
-  face = loadImage(int(random(1, 5)) + ".png");  
+  // get random face
+  let n = int(random(1, 5)); // 1~4
+  let imgName = n + ".png";  
+  face = loadImage(imgName);
 }
 
 // 커비 몸통 얼굴 일단 표출
 function setup() {
   createCanvas(400, 400);
-  background('beige');
-  image(body, 0, 0);
-  image(face, 132, 134);
+
+  let button = createButton('ㅗ'); // 버튼 생성
+  button.position(10, 10); // 버튼 위치
+
+  // 버튼 누르면 랜덤 얼굴 바뀌기
+  button.mousePressed(() => {
+    let n = int(random(1, 5));
+    let imgName = n + ".png";
+    face = loadImage(imgName);
+  });  
 }
 
-
+function draw() {
+  background("beige");
+  image(body, 0, 0);
+  image(face, 132, 134);  
+}
