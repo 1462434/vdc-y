0cb7d06165c12b1309123e0da0b8aea1093203e0
Pushpull-Kirby <1462434@snu.ac.kr>
2025-09-17 20:50:04 +0900

    Initial commit

---
+++ mickey-exercise/sketch.js
@@ -0,0 +1,56 @@
+function setup() {
+  createCanvas(600, 800);
+  background(200);
+  noStroke();
+}
+
+function draw() {
+
+  // background(mouseX/2.35);
+
+  // ears1
+  fill('black');
+  circle(300,86,126);
+
+  //face
+  fill('#F4E2C2');
+  circle(341,230,270);
+
+  // ears2
+  fill('black');
+  circle(191,188,146);
+
+  //eyes
+  fill('black');
+  circle(366,205,44);
+  circle(417,205,44);
+
+  //nose
+  circle(468,244,66); 
+
+  //mouth
+  fill('pink');
+  circle(334,315,54);  
+
+  //body
+  fill('red');
+  circle(384,443,200);    
+
+  //buttons
+  fill('white');
+  circle(463,405,48);
+  circle(413,424,48);
+
+  //feet
+  fill('yellow');
+  circle(169,612,180);
+  circle(423,679,180);
+
+  const canvas = document.querySelector('canvas');
+const link = document.createElement('a');
+link.download = 'mydrawing.png';
+link.href = canvas.toDataURL('image/png');
+link.click();
+
+}
+
