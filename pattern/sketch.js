cf1ec955573de4688c2e7ec8b2c94545e0213168
Pushpull-Kirby <1462434@snu.ac.kr>
2025-09-30 15:59:11 +0900

    연습: 움직이는 패턴
    연습 과제 만들기

---
+++ pattern/sketch.js
@@ -0,0 +1,52 @@
+const GRID = 2;
+const CELL = 1080 / GRID;
+
+function setup() {
+ createCanvas(1080, 1080);
+ noStroke();
+  frameRate(5);//원하는 속도로 조절
+ 
+}
+
+function draw() {
+ background(255, 255, 220);
+ const cz = random(40, CELL);
+ const sz = random(40, CELL);
+
+ for (let gy = 0; gy < GRID; gy++) {
+
+   for (let gx = 0; gx < GRID; gx++) {
+     const cx = gx * CELL + CELL / 2;
+     const cy = gy * CELL + CELL / 2;
+
+     push();
+     blendMode(MULTIPLY);
+     fill('orange');
+     circle(cx, cy, cz);
+     pop();
+
+     push();
+     blendMode(MULTIPLY);
+     translate(cx, cy);
+     fill('pink');
+     rectMode(CENTER);
+     rect(0, 0, sz, sz);
+     pop();
+
+     push();
+      translate(width/2, height/2); // move origin to canvas center
+      rotate(random(TWO_PI));       // random rotation
+      const s = random(300, 500);   // side length
+      const h = (sqrt(3) / 2) * s;  // height of equilateral triangle
+      fill('cyan');
+      triangle(-s/2, h/3, s/2, h/3, 0, -2*h/3);
+      pop();
+   }
+ }
+}
+
+function keyPressed() {
+  if (key === 's') {
+    saveGif('mySketch', 5);
+  }
+}
